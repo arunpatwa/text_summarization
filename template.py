c3cb7731d95115c4f517be62d613453ed145f4c8
arunpatwa <arunspatwa.14@gmail.com>
2024-03-21 17:12:07 +0530

    requirements done && setup installation done

--- template.py
+++ template.py
@@ -12,6 +12,7 @@ list_of_files=[".github/workflows/.gitkeep/",
                f"src/{project_name}/utils/__init__.py",
                f"src/{project_name}/utils/common.py",
                f"src/{project_name}/config/__init__.py",
+               f"src/{project_name}/logging/__init__.py",
                f"src/{project_name}/config/configuration.py",
                f"src/{project_name}/pipeline/__init__.py",
                f"src/{project_name}/entity/__init__.py",
@@ -23,7 +24,7 @@ list_of_files=[".github/workflows/.gitkeep/",
                "Dockerfile",
                "requirements.txt",
                "setup.py",
-               "research/trials.ipynb"
+               "research/trials.ipynb",
                ]
 
 for file_path in list_of_files:
@@ -31,17 +32,14 @@ for file_path in list_of_files:
     
     filedir,filename=os.path.split(file_path)
     
-    if(filedir!=""):
+    if(filedir !=""):
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"creating directory : {filedir} with {filename}")
-    
-    # print("file_dir : ",filedir)
-    # print("filename : ",filename)
-    
-    if(not os.path.exists(file_path))  or (os.path.getsize(file_path)):
+       
+    if(not os.path.exists(file_path))  or (os.path.getsize(file_path)==0):
         with open(file_path,"w") as f:
+            pass
             logging.info(f"creating empty file : {filename} in directory : {file_path}")
-        pass
     else:
         logging.info(f"filename : {filename} already exist in the directory : {file_path} ")
             
