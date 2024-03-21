471feefbd0c2c37eb51e81d60f18f902265a0874
arunpatwa <arunspatwa.14@gmail.com>
2024-03-21 15:57:30 +0530

    created template and structure

---
+++ template.py
@@ -0,0 +1,48 @@
+import os 
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
+
+project_name="textSummarizer"
+
+list_of_files=[".github/workflows/.gitkeep/",
+               f"src/{project_name}/__init__.py",
+               f"src/{project_name}/components/__init__.py",
+               f"src/{project_name}/utils/__init__.py",
+               f"src/{project_name}/utils/common.py",
+               f"src/{project_name}/config/__init__.py",
+               f"src/{project_name}/config/configuration.py",
+               f"src/{project_name}/pipeline/__init__.py",
+               f"src/{project_name}/entity/__init__.py",
+               f"src/{project_name}/constants/__init__.py",
+               "config/config.yaml",
+               "params.yaml"
+               "app.py",
+               "main.py",
+               "Dockerfile",
+               "requirements.txt",
+               "setup.py",
+               "research/trials.ipynb"
+               ]
+
+for file_path in list_of_files:
+    file_path=Path(file_path)
+    
+    filedir,filename=os.path.split(file_path)
+    
+    if(filedir!=""):
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating directory : {filedir} with {filename}")
+    
+    # print("file_dir : ",filedir)
+    # print("filename : ",filename)
+    
+    if(not os.path.exists(file_path))  or (os.path.getsize(file_path)):
+        with open(file_path,"w") as f:
+            logging.info(f"creating empty file : {filename} in directory : {file_path}")
+        pass
+    else:
+        logging.info(f"filename : {filename} already exist in the directory : {file_path} ")
+            
+    
